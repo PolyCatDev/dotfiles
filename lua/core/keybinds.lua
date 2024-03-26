47f2c6fca2509a6a3898ffb9f99533bc9d6f1fbc
PolyCat <polycat@tuta.io>
2024-03-26 10:48:15 +0200

    changed Telescope keybinds

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -1,5 +1,5 @@
 -- Telescope Search
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-o>', builtin.find_files, {})
-vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-o>', builtin.live_grep, {})
 
