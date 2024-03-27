812f69c5d505a9c29a8d2edaad10a6a64932020c
PolyCat <polycat@tuta.io>
2024-03-27 18:30:43 +0200

    added JavaScript LSP

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls", "basedpyright"}
+                ensure_installed = {"lua_ls", "basedpyright", "quick_lint_js"}
             })
         end
     },
