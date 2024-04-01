62c5c12a3abc72169f989784d3a4fde58dc5839b
PolyCat <polycat@tuta.io>
2024-04-01 21:01:45 +0300

    renamed mason.lua to lsp-config.lua

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+	{ -- LSP Manager
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+
+	{ -- LSP Garbber
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "basedpyright", "quick_lint_js" },
+			})
+		end,
+	},
+
+	{ -- LSP Connector (LSP -> nVim)
+		"neovim/nvim-lspconfig",
+
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			-- Comment out these line if ur not using lsp auto-complete (lua/plugins/completion.lua)
+--			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- "capabilities" should be removed if not using LSP auto-complete
+			lspconfig.lua_ls.setup({  })
+			lspconfig.pyright.setup({  })
+		end,
+	},
+}
