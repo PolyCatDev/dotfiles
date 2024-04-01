e74aa1204a02558f16cf1821240994cca805f134
PolyCat <polycat@tuta.io>
2024-04-01 21:43:17 +0300

    added back python lsp but correctly configured this time :)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,7 +13,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "quick_lint_js" },
+				ensure_installed = { "lua_ls", "basedpyright", "quick_lint_js" },
 			})
 		end,
 	},
@@ -29,6 +29,7 @@ return {
 
 			-- "capabilities" should be removed if not using LSP auto-complete
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.basedpyright.setup({ capabilities = capabilities })
 			lspconfig.quick_lint_js.setup({ capabilities = capabilities })
 		end,
 	},
