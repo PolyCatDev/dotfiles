5cbcda6c9aeccc9e2ccf57507309275ca209127a
PolyCat <polycat@tuta.io>
2024-04-02 11:51:43 +0300

    added vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,14 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup()
+	{ -- Git effects in editor
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-		vim.keymap.set("n", "jp", ":Gitsigns preview_hunk<CR>", {})
-	end,
+			vim.keymap.set("n", "jp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
+
+	{ -- Git commands in editor
+		"tpope/vim-fugitive",
+	},
 }
