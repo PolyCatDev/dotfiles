228b15fe347ad33ead14d838421c903a3207d101
PolyCat <polycat@tuta.io>
2024-04-02 11:37:09 +0300

    reforamtted file

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,8 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function ()
-        require("gitsigns").setup()
-        
-        vim.keymap.set("n", "jp", ":Gitsigns preview_hunk<CR>", {})
-    end
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+
+		vim.keymap.set("n", "jp", ":Gitsigns preview_hunk<CR>", {})
+	end,
 }
