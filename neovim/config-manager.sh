0fd5fa6ee60a5e68778abbfd0c0a3a46b34fff80
PolyCat <contact.polycat@proton.me>
2024-11-17 22:54:21 +0200

    added config-maager.sh script and changed telescope binds to use leader

---
+++ neovim/config-manager.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Config
+PLATFORM="github.com"
+NAME="polycatdev"
+REPO="dotfiles"
+DIR_NAME="neovim"
+
+# Functions
+printError () {
+    echo -e "\033[31mERROR: $1 \033[0m"
+}
+
+if [[ -d $REPO ]]; then
+    printError "Directory '$REPO' already exists"
+    exit 1
+fi
+
+# Instructions
+git clone "https://$PLATFORM/$NAME/$REPO.git"
+rm -rf "$REPO/$DIR_NAME/*"
+
+mkdir -p "$REPO/$DIR_NAME"
+
+cp -r $(ls | grep -v $REPO) $REPO/$DIR_NAME
+
+if [[ -n "$1" ]]; then
+    
+    if [[ "$1" == "-c" || "$1" == "-commit" ]] && [[ -n "$2" ]]; then
+
+        cd $REPO
+        git add .
+        git commit -m "$2"
+    
+        if [[ $3 == "-p" ]]; then
+            git push
+        fi
+
+        cd ..
+
+    else
+        printError "No commit message defined"
+        exit 1
+    fi
+fi
--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -5,8 +5,8 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-o>", builtin.live_grep, {})
+			vim.keymap.set("n", "<Leader>p", builtin.find_files, {})
+			vim.keymap.set("n", "<Leader>o", builtin.live_grep, {})
 		end,
 	},
 
