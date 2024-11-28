322a0084131ca2512a5e1d042174f6b7da6d52ed
PolyCat <contact.polycat@proton.me>
2024-11-28 18:22:50 +0200

    enchanced greet screen

--- neovim/lua/plugins/greet.lua
+++ neovim/lua/plugins/greet.lua
@@ -1,17 +1,28 @@
 return {
-	"PolyCatDev/dashboard-nvim",
-	event = "VimEnter",
-	config = function()
-		require("dashboard").setup({
-			-- config
-			theme = "hyper",
-			shuffle_letter = false,
-			config = {
-				shortcut = {},
-				packages = { enable = false },
-				footer = { "" },
-			},
-		})
-	end,
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    "PolyCatDev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+        require("dashboard").setup({
+            -- config
+            theme = "hyper",
+            shuffle_letter = false,
+            config = {
+                shortcut = {
+                    { desc = " Update", action = "Lazy update", key = "u" },
+                    { desc = " Files", action = "Telescope find_files", key = "p" },
+                    { desc= " Config", action = "Telescope find_files cwd=~/.config/nvim/", key = "c"},
+                },
+                project = {
+                    enable = true,
+                    limit = 8,
+                    icon = "  ",
+                    label = "Recent Projects:",
+                    action = "Telescope find_files cwd=",
+                },
+                packages = { enable = false },
+                footer = { "" },
+            },
+        })
+    end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
