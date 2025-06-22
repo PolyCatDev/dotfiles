2c08884628cd3840a3f50616f7a193fb948d2154
PolyCat <contact.polycat@proton.me>
2025-06-22 15:40:35 +0300

    added 2 spaces tab lenght for c and c++ files

--- nvim/.config/nvim/lua/core/config.lua
+++ nvim/.config/nvim/lua/core/config.lua
@@ -12,7 +12,7 @@ vim.o.wrap = false
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
--- Setup tabs in .gd files
+-- Setup tabs specific file types
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "gdscript",
     callback = function()
@@ -20,6 +20,16 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.tabstop = 4
         vim.opt_local.shiftwidth = 4
     end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "cpp" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
 })
 
 -- Keybinds
