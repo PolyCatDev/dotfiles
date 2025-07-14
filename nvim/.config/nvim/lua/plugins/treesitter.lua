ae4252c0274cd430c61c7775ef20f268049df9ba
PolyCat <contact.polycat@proton.me>
2025-07-14 13:50:28 +0300

    added new default laguages to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,30 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                auto_install = true,
-                ensure_installed = { "lua", "gdscript", "gdshader", "astro", "typescript", "css", "gitcommit", "go" },
+			configs.setup({
+				auto_install = true,
+				ensure_installed = {
+					"lua",
+					"gdscript",
+					"godot_resource",
+					"gdshader",
+					"astro",
+					"typescript",
+					"css",
+					"gitcommit",
+					"go",
+					"cpp",
+					"yaml",
+                    "markdown"
+				},
 
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
-    }
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
