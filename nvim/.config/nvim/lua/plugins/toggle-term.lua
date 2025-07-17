e8a3be47987b43aaf7e4937caf9fefcdc886226f
PolyCat <contact.polycat@proton.me>
2025-07-17 21:52:26 +0300

    updated config

--- nvim/.config/nvim/lua/plugins/toggle-term.lua
+++ nvim/.config/nvim/lua/plugins/toggle-term.lua
@@ -2,7 +2,7 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	opts = {
-		open_mapping = [[<c-`>]],
+		open_mapping = "<Leader>`",
 		size = 18,
 		shade_terminals = true,
 		shading_factor = -20,
