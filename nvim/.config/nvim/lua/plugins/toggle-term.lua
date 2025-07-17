8479f01103cf50e94ab331d8c152f68753d9c25e
PolyCat <contact.polycat@proton.me>
2025-07-17 21:54:27 +0300

    Revert "updated config"
    This reverts commit e8a3be47987b43aaf7e4937caf9fefcdc886226f.

--- nvim/.config/nvim/lua/plugins/toggle-term.lua
+++ nvim/.config/nvim/lua/plugins/toggle-term.lua
@@ -2,7 +2,7 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	opts = {
-		open_mapping = "<Leader>`",
+		open_mapping = [[<c-`>]],
 		size = 18,
 		shade_terminals = true,
 		shading_factor = -20,
