a63d66bb96d11605c4915b31f941f5ccf1e4dd96
PolyCat <contact.polycat@proton.me>
2025-07-26 16:21:11 +0300

    added better markdown support

--- nvim/.config/nvim/lua/core/config.lua
+++ nvim/.config/nvim/lua/core/config.lua
@@ -12,6 +12,27 @@ vim.o.wrap = false
 -- Use system clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Setup Markdown Logic
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+        vim.opt_local.wrap = true -- Enable soft wrapping
+        vim.opt_local.linebreak = true -- Don't break in the middle of a word
+
+        local opts = { buffer = true, noremap = true, silent = true }
+
+        vim.keymap.set("n", "j", "gj", opts)
+        vim.keymap.set("n", "k", "gk", opts)
+        vim.keymap.set("n", "<Down>", "gj", opts)
+        vim.keymap.set("n", "<Up>", "gk", opts)
+
+        vim.keymap.set("v", "j", "gj", opts)
+        vim.keymap.set("v", "k", "gk", opts)
+        vim.keymap.set("v", "<Down>", "gj", opts)
+        vim.keymap.set("v", "<Up>", "gk", opts)
+    end,
+})
+
 -- Setup Godot Logic
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "gdscript",
@@ -22,25 +43,24 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
-local gdproject = io.open(vim.fn.getcwd()..'/project.godot', 'r')
+local gdproject = io.open(vim.fn.getcwd() .. "/project.godot", "r")
 if gdproject then
     io.close(gdproject)
-    vim.fn.serverstart './godothost'
+    vim.fn.serverstart("./godothost")
 end
 
 -- Setup 2 space tabs for C and C++ files
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.expandtab = true
-  end,
+    pattern = { "c", "cpp" },
+    callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.expandtab = true
+    end,
 })
 
 -- Keybinds
-vim.api.nvim_set_keymap('n', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>", { noremap = true, silent = true })
-
