109473b871420d424293efaf30bf3e5fbb5c16c6
PolyCat <contact.polycat@proton.me>
2025-07-27 15:44:50 +0300

    configured markdown.nvim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,15 @@
 return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {
-        heading = {
-            sign = false,
-        },
-    },
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"echasnovski/mini.nvim",
+	},
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {
+		sign = { enabled = false },
+		heading = { enabled = false },
+		bullet = { enabled = false },
+		link = { enabled = false, render_modes = false },
+	},
 }
