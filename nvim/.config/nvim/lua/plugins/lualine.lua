6afe85204fd6f130bd8d9b47a8b60c64d6aca2b1
PolyCat <contact.polycat@proton.me>
2025-08-27 14:35:08 +0300

    Customized lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,8 +4,15 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-
 				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {},
+				lualine_b = { "branch" },
+				lualine_c = { "diagnostics", "filename" },
+				lualine_x = { "lsp_status", "filetype" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
