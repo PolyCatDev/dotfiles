45b04fc706f5e9b1847ca33e9c83762c507519d8
PolyCat <contact.polycat@proton.me>
2025-12-31 01:03:04 +0200

    Fixed breaking changes in Telescope and Treesitter plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,27 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-
-            configs.setup({
-                auto_install = true,
-                ensure_installed = {
-                    "lua",
-                    "gdscript",
-                    "godot_resource",
-                    "gdshader",
-                    "astro",
-                    "typescript",
-                    "css",
-                    "gitcommit",
-                    "git_rebase",
-                    "go",
-                    "cpp",
-                    "yaml",
-                    "markdown",
-                    "python",
-                    "ruby",
-                },
-
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		init = function()
+			vim.g.loaded_nvim_treesitter = 1 -- Disable nvim-treesitter's installation
+		end,
+	},
+	{
+		"lewis6991/ts-install.nvim",
+		lazy = false,
+		config = function()
+			require("ts-install").setup({
+				ensure_install = {
+					"lua",
+					"git_config",
+					"git_rebase",
+					"gitattributes",
+					"gitcommit",
+					"gitignore",
+				},
+				auto_install = true,
+			})
+		end,
+	},
 }
