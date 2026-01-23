d574cef2efdb8bada220649d5b92f087229a885f
PolyCat <contact.polycat@proton.me>
2026-01-23 10:17:04 +0200

    Placed $EDITOR between quotes

--- util-scripts/.local/bin/devup.sh
+++ util-scripts/.local/bin/devup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SESSION=$(basename "$PWD")
 
-tmux new-session -d -s "$SESSION" $EDITOR
+tmux new-session -d -s "$SESSION" "$EDITOR"
 tmux new-window -t "$SESSION"
 tmux select-window -t "$SESSION:1"
 tmux attach -t "$SESSION"
